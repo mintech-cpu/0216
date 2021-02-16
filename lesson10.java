bc9aa8ef93713249b143fa8892bbee36b45ccc30
mintech-cpu <minamiwada01@gmail.com>
2021-02-16 20:06:19 +0900

    initial commit

---
+++ lesson10.java
@@ -0,0 +1,15 @@
+// 条件分岐
+class lesson10{
+    public static void main(String[] args){
+        int age = 18;
+        if (age >= 20){
+            System.out.println("adult");
+        } else if (age==0) {
+            System.out.println("baby");
+        }
+        else {
+            System.out.println("child");
+        }
+
+    }
+}
